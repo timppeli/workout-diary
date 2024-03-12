747f2c706076bbc96e60a8bc0fa6559c294138ca
Tiina Pelimanni <tiina.pelimanni@gmail.com>
2024-03-12 23:33:25 +0200

    Radiobutton selections

--- screens/AddWorkout.js
+++ screens/AddWorkout.js
@@ -1,11 +1,40 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import Styles from '../styles/Styles';
+import React, { useState } from 'react';
+import { View, Text, TextInput } from 'react-native';
 
-export default function AddWorkout(){
-  return(
-      <View>
-          <Text>Add</Text>
-      </View>
+import Styles from '../styles/Styles';
+import RadioButton from '../components/RadioButton';
+
+const AddWorkout = () => {
+  const workoutTypes = [
+    { value: 'Run', icon: 'run' },
+    { value: 'Bike', icon: 'bike' },
+    { value: 'Swim', icon: 'swim' },
+  ];
+  const [distance, setDistance] = useState(0);
+  const [duration, setDuration] = useState(0);
+
+  return (
+    <View style={Styles.container}>
+      <Text style={Styles.label}>Workout Type:</Text>
+      <RadioButton data={workoutTypes} style={Styles.button}/>
+      <Text style={Styles.label}>Distance</Text>
+      <TextInput
+        style={Styles.input}
+        onChangeText={setDistance}
+        value={distance}
+        placeholder="Distance (km)"
+        keyboardType="numeric"
+      />
+      <Text style={Styles.label}>Duration</Text>
+      <TextInput
+        style={Styles.input}
+        onChangeText={setDuration}
+        value={duration}
+        placeholder="Duration (min)"
+        keyboardType="numeric"
+      />
+    </View>
   );
-}
+};
+
+export default AddWorkout;
--- styles/Styles.js
+++ styles/Styles.js
@@ -3,11 +3,44 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'orange',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'stretch',
+    justifyContent: 'start',
+    marginTop: 10,
+    padding: 20
   },
-  test: {
-    fontSize: 50,
-  }
+  input: {
+    padding: 10,
+    marginTop: 10,
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#666',
+    backgroundColor: "#fff"
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 10
+  },
+  option: {
+    color: '#111'
+  },
+  currentOption: {
+    color: 'white'
+  },
+  unselected: {
+    backgroundColor: 'orange',
+    margin: 5,
+    padding: 20,
+    borderRadius: 10
+  },
+  selected: {
+    backgroundColor: 'teal',
+    color: 'red',
+    margin: 6,
+    padding: 20,
+    borderRadius: 10
+  },
+  radiobuttons: {
+    flexDirection: "row",
+    justifyContent: "space-evenly"
+  },
 });
