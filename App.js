507e805969bd479c619b21b1effbd93c7c2224df
Tiina Pelimanni <tiina.pelimanni@gmail.com>
2024-03-12 21:08:00 +0200

    Bottom Tabs

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import AddExercise from './screens/AddExercise';
+import ExerciseHistory from './screens/ExerciseHistory';
+import Settings from './screens/Settings';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+
+      <Tab.Navigator>
+
+        <Tab.Screen name="Add Exercise" component={AddExercise} />
+
+        <Tab.Screen name="Exercise History" component={ExerciseHistory} />
+
+        <Tab.Screen name="Settings" component={Settings} />
+
+      </Tab.Navigator>
+
+    </NavigationContainer>
   );
 }
 
---
+++ screens/ExerciseHistory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ExerciseHistory = () => {
+  return (
+    <View>
+      <Text>History Screen</Text>
+    </View>
+  );
+};
+
+export default ExerciseHistory;
